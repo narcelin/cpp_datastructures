d15767082f5ac4e02598bd08f0914cfbd500e048
Nicolas Arcelin <nicolasarcelin@Nicolass-MacBook-Pro.local>
2024-02-02 09:36:49 -0500

    expanding to create user input for random value

--- discussions/random_number_generator.cpp
+++ discussions/random_number_generator.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cmath>
 #include <cstdlib>
 #include <ctime>
 
@@ -8,8 +7,14 @@ using namespace std;
 void RandomFloatingNumberGenerator(); // Random float Between 1 and 10
 
 int main(){
-    RandomFloatingNumberGenerator();
+    // RandomFloatingNumberGenerator();
 
+    char char_array[45];
+    cin >> char_array;
+
+    for(int i = 0; char_array[i] != '\0'; i++){
+        cout << char_array[i];
+    };
     return 0;
 }
 
@@ -17,11 +22,11 @@ void RandomFloatingNumberGenerator(){
     srand(time(0));
 
     string random_float = "";
-    int rand_int = (rand() % 10) + 1;
+    int rand_int = (rand() % 10) + 1; //Random int 1 - 10;
     random_float = to_string(rand_int);
     random_float += "."; //Adds decimal
 
-    for(int i = 0; i != 7; i++ ){ //Create a random int between 1-99 then concates it to the end of the string
+    for(int i = 0; i != 7; i++ ){ //Create a random int between 1 - 9 then concates it to the end of the string
         int rand_int = (rand() % 9) + 1;
         random_float += to_string(rand_int);
     }
