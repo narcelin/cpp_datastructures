0bb395c872aa661c197cbf877f9482f81c829496
Nicolas Arcelin <nicolasarcelin@Nicolass-MacBook-Pro.local>
2024-02-20 06:23:47 -0800

    zybooks folder created

---
+++ zybooks_labs/8.5_File_name_change.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstring>
+#include <fstream>
+
+// Include any necessary libraries here.
+
+using namespace std;
+
+int main() {
+   
+   /* Type your code here. */
+   ifstream inFS;
+   
+   string directoryName;
+   cin >> directoryName; 
+    
+   inFS.open("./"+directoryName);
+   
+   if(!inFS.is_open()){
+      cout << "Error opening file" << endl;
+      return 1;
+      };
+      
+   string fileName;
+   string strToReplace = "_photo.jpg";
+   inFS >> fileName;
+   
+   while(!inFS.fail()){
+      size_t strToReplaceIndex = fileName.find(strToReplace); //give fist index
+      
+      fileName.replace(strToReplaceIndex, strToReplace.length(), "_info.txt");
+      
+      cout << fileName << endl;
+      
+      inFS >> fileName;
+      
+      };
+
+   return 0;
+}
